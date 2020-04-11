3aeece07419d761b1c44f933029db1a066694a0c
caoguofeng <caoguofeng@droi.com>
2020-04-11 16:39:14 +0800

    第二章学习-快速入门Kotlin

--- MyApplication/app/src/main/java/com/example/myapplication/LearnKotlin.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/LearnKotlin.kt
@@ -7,7 +7,15 @@ fun main() {
     a = a * 10
     a = a * methodName(1,1)
     a = getMax(100, 32)
+    a = getMaxIf(20, 21)
     println("a = " + a)
+    println(getScore("xiaodu"))
+    println(checkNumber(111))
+    testFor()
+    val person = Person()
+    person.name = "小杜"
+    person.age = 19
+    person.eat()
 }
 fun methodName(param1:Int, param2:Int) : Int {
     return 0
@@ -15,4 +23,24 @@ fun methodName(param1:Int, param2:Int) : Int {
 
 fun getMax(num1: Int, num2: Int): Int{
     return max(num1, num2)
+}
+
+fun getMaxIf(num1: Int, num2: Int) = if(num1 < num2) num2 else num1
+
+fun getScore(name: String) = when (name) {
+    "xiaodu" -> 59
+    "xiaocao" -> 99
+    else -> 0
+}
+
+fun checkNumber(num: Number) = when(num) {
+    is Long -> "长整型"
+    is Int -> "整形"
+    else -> "不知道啥东西"
+}
+
+fun testFor() {
+    for (i in 10 downTo 1 step 5){
+        println(i)
+    }
 }
---
+++ MyApplication/app/src/main/java/com/example/myapplication/Person.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+class Person {
+    var name: String = ""
+    var age: Int = 0
+    fun eat() {
+        println(name + "正在吃东西，她今年 " + age + "岁了！")
+    }
+}
